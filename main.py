6e96ecf77a8edba657845a605bbc7174150b1d8f
Stefan van der Duijs <stefanvanderduijs@icloud.com>
2024-01-19 14:23:28 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Before running python files, run
+# .\venv\Scripts\Activate
+# in the command prompt to activate python
+# you can now run any python file with
+# python <filename>.py
+
 def main():
     print("Hello world!")
 
