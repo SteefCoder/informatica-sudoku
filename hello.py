0511687ea27037472f0f1eb35f88ade9be3cc8ed
SteefCoder <140918585+SteefCoder@users.noreply.github.com>
2024-02-09 14:15:45 +0100

    Added solver endpoint to API

--- hello.py
+++ hello.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from sudoku_solver import solve_sudoku
 
 app = Flask(__name__)
+app.secret_key = rb'43290&O*#@&$#@*Ofk#$#@#*^TIF$@#&B*'
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template("index.html")
 
-app.run(debug=True)
+
+@app.route('/solve-sudoku', methods=['GET'])
+def solve():
+    grid = request.args["grid"]
+    if not all(x.isdigit() for x in grid):
+        return {"grid": "", "solvable": False}
+    
+    int_grid = [int(x) for x in grid]
+    solvable, solution = solve_sudoku(int_grid)
+    return {"grid": solution, "solvable": solvable}
