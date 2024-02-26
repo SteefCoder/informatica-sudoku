6e66ea2b2ed5b876630d0176928574e51d89d281
Stefan van der Duijs <stefanvanderduijs@icloud.com>
2024-02-26 13:45:00 +0100

    Updated C binding

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -3,22 +3,22 @@ This is a binding to the solver.c sudoku solver
 The function solve_sudoku defined in here just calls the same-named c function.
 """
 
-#import ctypes
+import ctypes
 
-#CHAR_81 = ctypes.c_char * 81
+INT_81 = ctypes.c_int * 81
 
 # load the compiled c file
-#_solverlib = ctypes.CDLL("./solverlib.so")
+_solverlib = ctypes.CDLL("./solverlib.so", winmode=0)
 
 # set up the c function
-#_solve = _solverlib.solve_sudoku
-#_solve.argtypes = [CHAR_81]
-#_solve.restype = ctypes.c_bool
+_solve = _solverlib.solveSudoku2
+_solve.argtypes = [INT_81]
 
 def solve_sudoku(grid: list[int]) -> tuple[bool, list[int]]:
     """Takes in the code and returns the solution and whether it can be solved.
-    If the sudoku cannot be solved, the returned solution could be anything."""
-    # str_pointer = CHAR_81(*map(ord, code))
-    # solved = _solve(str_pointer)
-    # return solved, str_pointer.value
-    return True, list(range(1, 10)) * 9
+    If the sudoku cannot be solved, the returned solution should be the original sudoku."""
+    array = INT_81(*grid)
+    _solve(array)
+    solved_grid = [int(x) for x in array.value]
+    solvable = all(x != 0 for x in solved_grid)
+    return solvable, solved_grid
