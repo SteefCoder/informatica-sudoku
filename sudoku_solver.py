7694a3c1f0597f7b5b87920ce1dae55f45efcc76
Stefan van der Duijs <stefanvanderduijs@icloud.com>
2024-02-29 09:11:56 +0100

    Delete unnecessary files

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -3,22 +3,23 @@ This is a binding to the solver.c sudoku solver
 The function solve_sudoku defined in here just calls the same-named c function.
 """
 
-#import ctypes
+import ctypes
 
-#CHAR_81 = ctypes.c_char * 81
+INT_81 = ctypes.c_int * 81
 
 # load the compiled c file
-#_solverlib = ctypes.CDLL("./solverlib.so")
+_solverlib = ctypes.CDLL("./solverlib.so")
 
 # set up the c function
-#_solve = _solverlib.solve_sudoku
-#_solve.argtypes = [CHAR_81]
-#_solve.restype = ctypes.c_bool
+_solve = _solverlib.solveSudoku
+_solve.argtypes = [INT_81]
+# _solve.restype = ctypes.c_bool
 
 def solve_sudoku(grid: list[int]) -> tuple[bool, list[int]]:
     """Takes in the code and returns the solution and whether it can be solved.
     If the sudoku cannot be solved, the returned solution could be anything."""
-    # str_pointer = CHAR_81(*map(ord, code))
-    # solved = _solve(str_pointer)
-    # return solved, str_pointer.value
-    return True, list(range(1, 10)) * 9
+    int_pointer = INT_81(*grid)
+    _solve(int_pointer)
+    sudoku = list(int_pointer.value)
+    solved = all(x != 0 for x in sudoku)
+    return solved, sudoku
